13e28f45335c23c8749d277c48c749ad14d78994
Ionică Bizău <bizauionica@yahoo.com>
2014-07-22 18:38:12 +0300

    new test

--- test/index.js
+++ test/index.js
@@ -2,3 +2,20 @@ var Box = require("../index");
 
 var b1 = new Box("20x10");
 console.log(b1.toString());
+
+var b2 = new Box({
+    w: 20
+  , h: 20
+  , marks: {
+        nw: "╔"
+      , n:  "══"
+      , ne: "╗"
+      , e:  "║"
+      , se: "╝"
+      , s:  "══"
+      , sw: "╚"
+      , w:  "║"
+      , b: "░░"
+    }
+});
+console.log(b2.toString());
