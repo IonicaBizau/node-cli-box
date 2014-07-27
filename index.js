062c25e62a3fd130af554b0d760a5086f2c4b6f1
Ionică Bizău <bizauionica@yahoo.com>
2014-07-27 09:44:10 +0300

    Unified code style. 80 column limit

--- index.js
+++ index.js
@@ -43,12 +43,13 @@ module.exports = function (options, text) {
             marks: {}
         }
     }
-    
+
+    // Handle text parameter
     if (text) {
-        
-        var alignTextVertically = function(splits, mode) {
+
+        var alignTextVertically = function (splits, mode) {
             if (splits.length > h && !mode) mode = "top";
-            mode = (["middle", "top", "bottom"].indexOf(mode) > -1) ? mode : "middle";
+            mode = (["top", "bottom"].indexOf(mode) > -1) ? mode : "middle";
 
             if (mode == "middle") {
                 return Math.floor((h / 2) - (splits.length / 2));
@@ -57,20 +58,21 @@ module.exports = function (options, text) {
             } else if (mode == "bottom") {
                 return h - splits.length;
             }
+        };
 
-        }
-
-        var alignLineHorizontally = function(line, mode) {
-            mode = (["center", "left", "right"].indexOf(mode) > -1) ? mode : "center";
+        var alignLineHorizontally = function (line, mode) {
+            mode = (["left", "right"].indexOf(mode) > -1) ? mode : "center";
 
             if (mode == "center") {
-                line.offset.x = parseInt(((w - 2) / 2) - (line.text.length / 2));
+                line.offset.x = parseInt(
+                    ((w - 2) / 2) - (line.text.length / 2)
+                );
             } else if (mode == "left") {
                 line.offset.x = 0;
             } else if (mode == "right") {
                 line.offset.x = (w - 2) - line.text.length;
             }
-            
+
             if (line.offset.x < 0) line.offset.x = 0;
 
             // Handle overflowing text
@@ -78,36 +80,47 @@ module.exports = function (options, text) {
                 line.text = line.text.substr(0, w - 5) + "...";
             }
             return line;
-        }
-        
-        var line, 
-            textOffsetY;
-        
+        };
+
+        var line
+          , textOffsetY
+          ;
+
         // Divide text into lines and calculate position
         if (typeof text === "string") {
-            var splits = text.split('\n').map(function(val) { return val.trim(); });
+
+            var splits = text.split("\n").map(function (val) {
+                return val.trim();
+            });
 
             textOffsetY = alignTextVertically(splits);
 
-            for (var i = 0; i < splits.length; i++) {
+            for (var i = 0; i < splits.length; ++i) {
                 line = {
-                    text: splits[i],
-                    offset: {
-                      y: textOffsetY + i
+                    text: splits[i]
+                  , offset: {
+                        y: textOffsetY + i
                     }
                 };
                 line = alignLineHorizontally(line);
                 lines.push(line);
             }
         } else if (typeof text === "object") {
+
             var stretch = text.stretch || false
               , autoEOL = text.autoEOL || false
               , hAlign = text.hAlign || undefined
               , vAlign = text.vAlign || undefined
-              , splits = text.text.split('\n').map(function(val) { return val.trim(); });
+              , splits = text.text.split("\n").map(function (val) {
+                    return val.trim();
+                })
+              ;
+
             // Stretch box to fit text (or console)
             if (stretch) {
-                var longest = splits.reduce(function (prev, curr) { return (prev.length > curr.length) ? prev : curr }).length;
+                var longest = splits.reduce(function (prev, curr) {
+                    return (prev.length > curr.length) ? prev : curr;
+                }).length;
                 if (longest > (w - 2)) {
                     if ((longest - 2) > process.stdout.columns) {
                         w = process.stdout.columns;
@@ -117,45 +130,47 @@ module.exports = function (options, text) {
                 }
                 h = (splits.length > h) ? splits.length : h;
             }
-            
+
             // Break lines automatically
             if (autoEOL) {
-                for(var i = 0; i < splits.length; i++) {
+                for(var i = 0; i < splits.length; ++i) {
                     // If too long to fit
                     if(splits[i].length > (w - 2)) {
                         // Find a place to break line
                         var ii = w - 2;
                         while(ii > 0 && splits[i][ii] != " ") {
-                            ii--;
+                            --ii;
                         }
                         if (ii == 0) {
                             ii = w - 2;
-                            while (ii < splits[i].length && splits[i][ii] != " ") {
-                                ii++;
+                            while (ii < splits[i].length
+                                   && splits[i][ii] != " ") {
+                                ++ii;
                             }
                         }
                         // Divide line
                         if(ii > 0 && ii < splits[i].length) {
-                            var div1 = splits[i].substr(0, ii);
-                            var div2 = splits[i].slice(ii+1);
+                            var div1 = splits[i].substr(0, ii)
+                              , div2 = splits[i].slice(ii+1)
+                              ;
                             splits.splice(i, 1, div1, div2);
                         }
                     }
                 }
             }
-            
+
             // Recalculate line number if necessary
             if (stretch) h = (splits.length > h) ? splits.length : h;
-            
+
             // Get vertical text offset
             textOffsetY = alignTextVertically(splits, vAlign);
-            
+
             // Push lines
-            for (var i = 0; i < splits.length; i++) {
+            for (var i = 0; i < splits.length; ++i) {
                 line = {
-                    text: splits[i],
-                    offset: {
-                      y: textOffsetY + i
+                    text: splits[i]
+                  , offset: {
+                        y: textOffsetY + i
                     }
                 };
                 line = alignLineHorizontally(line, hAlign);
@@ -205,25 +220,32 @@ module.exports = function (options, text) {
         box += this.settings.marks.ne;
 
         // The other lines
-        var nextLine = (this.settings.lines.length) ? this.settings.lines.shift() : undefined;
-            
+        var nextLine = this.settings.lines.length
+                       ? this.settings.lines.shift() : undefined;
+
         for (var i = 0; i < this.settings.height; ++i) {
-            
+
             // Get next line to display if one exists
-            while (nextLine && i > nextLine.offset.y && this.settings.lines.length) {
+            while (nextLine && i > nextLine.offset.y
+                   && this.settings.lines.length) {
                 nextLine = this.settings.lines.shift();
             }
-            
+
             box += OS.EOL + this.settings.marks.w;
-            
+
             for (var ii = 0; ii < this.settings.width - 2; ++ii) {
-                
-                if (nextLine                                            // there is something to display
-                    && i == nextLine.offset.y                           // it's the correct line
-                    && ii >= nextLine.offset.x                          // it's after the x offset
-                    && ii < (nextLine.offset.x + nextLine.text.length)  // the text hasn't ended yet
-                    && nextLine.text[ii - nextLine.offset.x] != " ") {  // it's not a whitespace
-                    
+
+                // there is something to display
+                if (nextLine
+                    // it's the correct line
+                    && i == nextLine.offset.y
+                    // it's after the x offset
+                    && ii >= nextLine.offset.x
+                    // the text hasn't ended yet
+                    && ii < (nextLine.offset.x + nextLine.text.length)
+                    // it's not a whitespace
+                    && nextLine.text[ii - nextLine.offset.x] != " ") {
+
                     box += nextLine.text[ii - nextLine.offset.x];
                 } else {
                     box += this.settings.marks.b;
