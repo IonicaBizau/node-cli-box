7f81f9c610fe91d4afdf0bff1f8c486fc48301fe
Ionică Bizău <bizauionica@yahoo.com>
2014-07-27 09:52:40 +0300

    Comments

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
+// Dependencies
 var Box = require("../index");
 
+// Create a simple box
 var b1 = new Box("20x10");
 console.log(b1.toString());
 
+// Set marks
 var b2 = new Box({
     w: 20
   , h: 20
@@ -20,9 +23,11 @@ var b2 = new Box({
 });
 console.log(b2.toString());
 
+// Box with text
 var b3 = new Box("20x40", "I will be displayed inside! \n And I'm in a new line!");
 console.log(b3.toString());
 
+// Box with aligned text to top-right
 var b4 = new Box("30x20", {
     text: "Box content"
   , stretch: true
