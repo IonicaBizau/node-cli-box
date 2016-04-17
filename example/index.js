c8ed0075e9e1f32027df5f42b7cd552c0ed6d682
Ionică Bizău <bizauionica@gmail.com>
2016-04-17 11:49:12 +0300

    :rocket:

--- example/index.js
+++ example/index.js
@@ -24,6 +24,7 @@ var b2 = new Box({
 });
 console.log(b2.stringify());
 
+
 // Box with text and use the stringify
 var b3 = Box("20x10", "I will be \u001b[31mdis\u001b[0mplayed inside! \n A\u001b[34mnd I'm in a\u001b[0m new line!");
 console.log(b3);
